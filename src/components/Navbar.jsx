2c6e9b2bb89c55391b1442cd6c31743843c35a7e
Venkatesh S S <vetrivenky2020@gmail.com>
2025-01-06 17:19:20 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
         <nav className="bg-white shadow-md">
             <div className='container mx-auto px-4 md:px-16 lg:px-24 py-4 flex justify-between items-center'>
                 <div className='text-2xl font-bold'>
-                    <Link to="/">Online-KADAI</Link>
+                    <Link to="/">Spartan store</Link>
                 </div>
                 <div className='relative flex-1 mx-4'>
                     <form onSubmit={handleSearch} >
